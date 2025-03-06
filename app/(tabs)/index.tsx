204cd43359f164be5e2251e7d0f6d42ced9d5665
Jivan Kadel <jivankadel256@gmail.com>
2025-03-06 17:12:20 +0545

    create the expo app

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+     >
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  stepContainer: {
+      gap: 8,
+    marginBottom: 8,
+  },
+});
